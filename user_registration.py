62db4fe94f37b8ef2854831e2c6469c3fd48dc18
Aniketwagh123 <106876220+Aniketwagh123@users.noreply.github.com>
2024-07-24 06:01:01 +0530

    [Aniket wagh]Feat: take valid First Name input for user

--- user_registration.py
+++ user_registration.py
@@ -1,6 +1,32 @@
+class User:
+    def __init__(self, name):
+        self.__first_name = name
+
+    def __str__(self):
+        return self.__first_name
+
+
 class UserValidator:
-    pass
+
+    @classmethod
+    def validate_first_name(cls, first_name: str) -> bool:
+        if len(first_name) >= 3 and first_name.istitle():
+            return True
+        return False
+
+
+class InputReader:
+    @classmethod
+    def name_input(cls) -> str:
+        while True:
+            first_name = input("Enter your first name: ")
+            if UserValidator.validate_first_name(first_name):
+                return first_name
+            else:
+                print("First name must be at least 3 characters long. Please try again.")
 
 
 if __name__ == "__main__":
-    pass
+    first_name = InputReader.name_input()
+    user = User(first_name)
+    print(user)
