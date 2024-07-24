cfabc9ec58380a6c9417c219e176a04c9a0a624d
Aniketwagh123 <106876220+Aniketwagh123@users.noreply.github.com>
2024-07-24 06:25:00 +0530

    [Aniket wagh]Feat: take valid mobile number input for user

--- user_registration.py
+++ user_registration.py
@@ -2,16 +2,18 @@ import re
 
 
 class User:
-    def __init__(self, first_name: str, last_name: str, email: str):
+    def __init__(self, first_name: str, last_name: str, email: str, mobile_number: str):
         self.__first_name = first_name
         self.__last_name = last_name
         self.__email = email
+        self.__mobile_number = mobile_number
 
     def __str__(self):
-        return f"[First Name: {self.__first_name}, Last Name: {self.__last_name}, Email: {self.__email}]"
+        return f"[First Name: {self.__first_name}, Last Name: {self.__last_name}, Email: {self.__email}, Mobile Number: {self.__mobile_number}]"
 
 
 class UserValidator:
+
     @classmethod
     def validate_last_name(cls, last_name: str) -> bool:
         if len(last_name) >= 3 and last_name.istitle():
@@ -26,15 +28,24 @@ class UserValidator:
 
     @classmethod
     def validate_email(cls, email: str) -> bool:
-        pattern = r'^[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)?@[a-zA-Z0-9]+\.[a-zA-Z]{2,}(?:\.[a-zA-Z]{2,})?$'
-        return re.match(pattern, email) is not None
+        email_regex = r'^[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)?@[a-zA-Z0-9]+\.[a-zA-Z]{2,3}(\.[a-zA-Z]{2})?$'
+        if re.match(email_regex, email):
+            return True
+        return False
+
+    @classmethod
+    def validate_mobile_number(cls, mobile_number: str) -> bool:
+        mobile_regex = r'^\d{2} \d{10}$'
+        if re.match(mobile_regex, mobile_number):
+            return True
+        return False
 
 
 class InputReader:
     @classmethod
     def first_name_input(cls) -> str:
         while True:
-            first_name = input("Enter your first name (e.g., Aniket): ")
+            first_name = input("Enter your first name (e.g. Aniket): ")
             if UserValidator.validate_first_name(first_name):
                 return first_name
             else:
@@ -44,7 +55,7 @@ class InputReader:
     @classmethod
     def last_name_input(cls) -> str:
         while True:
-            last_name = input("Enter your last name (e.g., Wagh): ")
+            last_name = input("Enter your last name (e.g. Wagh): ")
             if UserValidator.validate_last_name(last_name):
                 return last_name
             else:
@@ -54,17 +65,29 @@ class InputReader:
     @classmethod
     def email_input(cls) -> str:
         while True:
-            email = input("Enter your email (e.g., abc.xyz@bl.co.in): ")
+            email = input("Enter your email (e.g. abc.xyz@bl.co.in): ")
             if UserValidator.validate_email(email):
                 return email
             else:
+                print("Invalid email format. Please try again.")
+
+    @classmethod
+    def mobile_number_input(cls) -> str:
+        while True:
+            mobile_number = input(
+                "Enter your mobile number (e.g. 91 9919819801): ")
+            if UserValidator.validate_mobile_number(mobile_number):
+                return mobile_number
+            else:
                 print(
-                    "Email must be in the format abc.xyz@bl.co.in with precise @ and . positions. Please try again.")
+                    "Mobile number must be in the format '91 9919819801'. Please try again.")
 
 
 if __name__ == "__main__":
     first_name = InputReader.first_name_input()
     last_name = InputReader.last_name_input()
     email = InputReader.email_input()
-    user = User(first_name=first_name, last_name=last_name, email=email)
+    mobile_number = InputReader.mobile_number_input()
+    user = User(first_name=first_name, last_name=last_name,
+                email=email, mobile_number=mobile_number)
     print(user)
